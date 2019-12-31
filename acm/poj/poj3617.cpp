3b37872f1084662f21d1db26da6de2b4b4130cdd
DDF-ZTT <a1157790064@gmail.com>
2019-12-31 14:07:35 +0800

    solve 3617 小心PE

--- acm/poj/poj3617.cpp
+++ acm/poj/poj3617.cpp
@@ -4,32 +4,46 @@
  */
 
 #include <iostream>
-
 using namespace std;
 
 const int MAXN = 2000 + 10;
 char s[MAXN];
 int n;
+
 void solve() {
     std::ios::sync_with_stdio(false);
     while (cin>>n) {
        for (int i = 0; i < n; i++) {
-           cin>>s[i];
+           cin >> s[i];
        }
-       string ans;
-       int l = 0;
-       int r = n - 1;
-        for (int i = 0; l + i <= r; i++) {
-            if (s[l + i] > s[r - i]) {
-                ans += s[r-i];
-                r--;
-
-            } else if(s[l + i] <= s[r - i]) {
-                ans += s[l+i];
-                l++;
-            }
-        }
-       cout<<ans<<endl;
+       int a = 0;
+       int b = n - 1;
+       int cnt = 0;
+       while(a<=b) {
+           bool flag = true;
+           for (int i = 0; a + i <= b;i++){
+               if (s[a + i] < s[b - i]) {
+                   flag = true;
+                   break;
+               } else if (s[a+i] > s[b-i]) {
+                   flag = false;
+                   break;
+               }
+           }
+           if (flag) {
+               cout<<s[a++];
+           } else {
+               cout<<s[b--];
+           }
+           cnt++;
+           if (cnt == 80) {
+               cnt = 0;
+               cout<<endl;
+           }
+       }
+       if (cnt != 0) {
+           cout<<endl;
+       }
     }
 }
 int main() {
